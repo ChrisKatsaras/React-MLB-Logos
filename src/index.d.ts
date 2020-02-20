582e7842fb52860befef568a23050748db195416
Christopher Katsaras <chriskatsaras5@gmail.com>
2020-02-20 00:21:01 -0500

    Added base files

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+import { ComponentType, SVGAttributes } from 'react';
+
+interface Props extends SVGAttributes<SVGElement> {
+  size?: string | number;
+}
+
+type Icon = ComponentType<Props>;
