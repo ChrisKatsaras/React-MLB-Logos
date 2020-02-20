720ef7c5535b30a693f3788d75b9a0d64564d28e
Christopher Katsaras <chriskatsaras5@gmail.com>
2020-02-20 00:37:45 -0500

    Updated index files to include all logos
    Logos will be added next

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,3 +6,34 @@ interface Props extends SVGAttributes<SVGElement> {
 }
 
 type Icon = ComponentType<Props>;
+export const ARI: Icon;
+export const ATL: Icon;
+export const BAL: Icon;
+export const BOS: Icon;
+export const CHC: Icon;
+export const CHW: Icon;
+export const CIN: Icon;
+export const CLE: Icon;
+export const COL: Icon;
+export const DET: Icon;
+export const FLA: Icon;
+export const HOU: Icon;
+export const KAN: Icon;
+export const LAA: Icon;
+export const LAD: Icon;
+export const MIL: Icon;
+export const MIN: Icon;
+export const NYM: Icon;
+export const NYY: Icon;
+export const OAK: Icon;
+export const PHI: Icon;
+export const PIT: Icon;
+export const SD: Icon;
+export const SF: Icon;
+export const SEA: Icon;
+export const STL: Icon;
+export const TB: Icon;
+export const TEX: Icon;
+export const TOR: Icon;
+export const WAS: Icon;
+export const MLB: Icon;
